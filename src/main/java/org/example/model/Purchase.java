9bf06e72b806663e0008e55e526ccb0946345733
Gary A. Stafford <gary.a.stafford@gmail.com>
2022-09-10 11:09:48 -0400

    Impliment serializable in Purchase class

--- src/main/java/org/example/model/Purchase.java
+++ src/main/java/org/example/model/Purchase.java
@@ -5,12 +5,13 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Purchase {
+public class Purchase implements Serializable {
     @SerializedName("transaction_time")
     private String transactionTime;
 
